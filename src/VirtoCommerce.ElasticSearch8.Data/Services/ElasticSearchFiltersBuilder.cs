a213ddc603dc419f2b453685d9e4754d714da49f
Oleg Zhuk <zhukoo@gmail.com>
2025-12-22 16:47:07 +0200

    VCST-4471: NullReferenceException in ElasticSearchFiltersBuilder.GetFilterQuery
    fix: NullReferenceException in ElasticSearchFiltersBuilder.GetFilterQuery is TermFilter has null in Values.

--- src/VirtoCommerce.ElasticSearch8.Data/Services/ElasticSearchFiltersBuilder.cs
+++ src/VirtoCommerce.ElasticSearch8.Data/Services/ElasticSearchFiltersBuilder.cs
@@ -15,6 +15,9 @@ namespace VirtoCommerce.ElasticSearch8.Data.Services
     {
         public virtual Query GetFilterQuery(IFilter filter, IDictionary<PropertyName, IProperty> availableFields)
         {
+            ArgumentNullException.ThrowIfNull(filter);
+            ArgumentNullException.ThrowIfNull(availableFields);
+
             Query result = null;
 
             switch (filter)
@@ -60,7 +63,7 @@ namespace VirtoCommerce.ElasticSearch8.Data.Services
         protected virtual bool HasWildcardValue(TermFilter termFilter)
         {
             return termFilter.Values is { Count: 1 } &&
-                   termFilter.Values.Any(v => v.Contains('*') || v.Contains('?'));
+                termFilter.Values.Where(v => v != null).Any(v => v.Contains('*') || v.Contains('?'));
         }
 
         protected virtual IdsQuery CreateIdsFilter(IdsFilter idsFilter)
@@ -95,7 +98,7 @@ namespace VirtoCommerce.ElasticSearch8.Data.Services
             }
             else
             {
-                termValues = termFilter.Values.Select(x => FieldValue.String(x.ToLowerInvariant())).ToArray();
+                termValues = termFilter.Values.Select(x => FieldValue.String(x?.ToLowerInvariant())).ToArray();
             }
 
             return new TermsQuery
